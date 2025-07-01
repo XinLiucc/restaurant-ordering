6ef6137c7b0d2868f0e1a674500d023d6dc2e66e
Xiu Lin <lxin233@163.com>
2025-07-01 15:36:24 +0800

    app.js

--- restaurant-ordering-system/admin-web/src/stores/app.js
+++ restaurant-ordering-system/admin-web/src/stores/app.js
@@ -33,19 +33,3 @@ export const useAppStore = defineStore('app', () => {
     setTheme
   }
 })
-
-// src/api/index.js
-import request from '@/utils/request'
-
-/**
- * 通用API封装
- */
-export const api = {
-  get: (url, params) => request.get(url, { params }),
-  post: (url, data) => request.post(url, data),
-  put: (url, data) => request.put(url, data),
-  delete: (url) => request.delete(url),
-  patch: (url, data) => request.patch(url, data)
-}
-
-export default api
